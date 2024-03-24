95972c8da91573ff06df96d215a164977e264112
TANG CHENG LEE <TANGCHENGLEE@virgil246.com>
2024-03-24 18:27:41 +0800

    Add Card Component

---
+++ components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-900 p-4 rounded-lg text-white">
+    <div>
+      <h3 class="text-xl font-semibold mb-3">{{ props.Info.Title }}</h3>
+      <p>{{ props.Info.Description }}</p>
+    </div>
+    <slot name="List"> </slot>
+    <slot name="Bottom"> </slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["Info"]);
+</script>
+
+<style></style>
